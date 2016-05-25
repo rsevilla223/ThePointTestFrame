1a2d82cae4a410b6c4f6ebe66d669ca9298c8fe3
PSLoaner4 <psloaner4@PSLoaner4s-MacBook-Pro.local>
2016-05-25 16:11:09 -0500

    added cool floating fab button.

--- App/app.js
+++ App/app.js
@@ -1,5 +1,5 @@
 ﻿
-var app = angular.module('App', ['ui.router', 'ngMaterial', 'ngMessages', 'ngMdIcons']);
+var app = angular.module('App', ['ui.router', 'ngMaterial', 'ngMessages', 'ngMdIcons', 'ng-mfb']);
 
 app.config(function ($stateProvider,$urlRouterProvider) {
 
